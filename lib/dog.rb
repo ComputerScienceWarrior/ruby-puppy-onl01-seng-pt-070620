c3c1b802b89bd68c1215c7e5e97f785d182e0cb8
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-22 22:50:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Add your code here
+class Dog 
+  attr_accessor :name
+  @@all = []#store all instances of Dog class
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
