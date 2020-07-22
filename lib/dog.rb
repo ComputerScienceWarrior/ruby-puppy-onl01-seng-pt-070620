6cdd3e3add7842b2321216c5af42e0daf0357e54
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-22 22:56:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
@@ -17,6 +17,6 @@ class Dog
   end
   
   def save
-    
+    @@all << self
   end
 end
